0767614d0ae07f545dfe0070480c7e505d73757d
guynachum1122 <guy.nachum1122@gmail.com>
2026-07-14 23:54:41 +0300

    add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import { withSentryConfig } from '@sentry/nextjs';
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  trailingSlash: false,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: '**.unsplash.com' },
+    ],
+  },
+  experimental: {
+    instrumentationHook: true,
+  },
+};
+
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
