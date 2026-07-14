01d75192ed1ea34fca850628b37db3e8e80f3d5f
guynachum1122 <guy.nachum1122@gmail.com>
2026-07-14 23:55:07 +0300

    add app/(storefront)/layout.tsx

---
+++ app/(storefront)/layout.tsx
@@ -0,0 +1,14 @@
+import { SiteHeader } from '@/components/layout/site-header';
+import { SiteFooter } from '@/components/layout/site-footer';
+import { CookieConsentBanner } from '@/components/compliance/cookie-consent';
+
+export default function StorefrontLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <SiteHeader />
+      <main className="flex-1">{children}</main>
+      <SiteFooter />
+      <CookieConsentBanner />
+    </div>
+  );
+}
