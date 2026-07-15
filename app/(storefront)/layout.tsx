a6a49b27993a90dbca363a84320051a8d5147097
guynachum1122 <guy.nachum1122@gmail.com>
2026-07-15 07:42:56 +0300

    update app/(storefront)/layout.tsx

--- app/(storefront)/layout.tsx
+++ app/(storefront)/layout.tsx
@@ -2,6 +2,8 @@ import { SiteHeader } from '@/components/layout/site-header';
 import { SiteFooter } from '@/components/layout/site-footer';
 import { CookieConsentBanner } from '@/components/compliance/cookie-consent';
 
+export const dynamic = 'force-dynamic';
+
 export default function StorefrontLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
